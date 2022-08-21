fdec08cfd5ae3564901651996801c9b78a40cbdf
Hhasan123 <hy907544@gmail.com>
2022-08-21 14:11:11 +0300

    Add project files.

---
+++ DictionaryStructure/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryStructure
+{
+    class MyDictionary<Key,Value>
+    {
+        Key[] keys;
+        Value[] values;
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+        }
+        public void Add(Key key,Value value)
+        {
+            Key[] tempKeyArray = keys;
+            keys = new Key[keys.Length+1];
+            Value[] tempValueArray = values;
+            values = new Value[values.Length + 1];
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                keys[i] = tempKeyArray[i];
+                values[i] = tempValueArray[i];
+
+            }
+            keys[keys.Length-1] = key;
+            values[values.Length-1] = value;
+
+        }
+        public int Length
+        {
+            get { return keys.Length; }
+        }
+    }
+}
---
+++ DictionaryStructure/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kullanicilar = new Dictionary<int, String>();
+            var isimler = new MyDictionary<int, String>();
+            Console.WriteLine(isimler.Length);
+            isimler.Add(0, "Sifir");
+            isimler.Add(1, "Bir");
+            isimler.Add(2, "Iki");
+            Console.WriteLine(isimler.Length);
+            Console.ReadLine();
+
+        }
+    }
+}
